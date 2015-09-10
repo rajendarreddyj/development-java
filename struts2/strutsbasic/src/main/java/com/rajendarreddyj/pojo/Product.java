bf86fa80ad936d8c0da70f49acb4f3f7ec2b6cb1
rajendarreddyj <rajendarreddy.jagapathi@gmail.com>
2015-09-09 22:08:28 -0500

    Initial Commit

---
+++ struts2/strutsbasic/src/main/java/com/rajendarreddyj/pojo/Product.java
@@ -0,0 +1,32 @@
+package com.rajendarreddyj.pojo;
+
+/**
+ * @author rajendarreddy
+ *
+ */
+public class Product {
+	private int id;  
+	private String name;  
+	private float price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public String execute(){
+		return "success";
+	}
+}
