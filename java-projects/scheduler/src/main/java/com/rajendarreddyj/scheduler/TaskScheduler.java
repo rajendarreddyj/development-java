4cb67f197dcdc8095dba6a52500334ddc404a4c5
rajendarreddy jagapathi <rajendarreddyj@gmail.com>
2017-01-23 11:52:04 -0600

    updating to create temp file to store file pointer

--- java-projects/scheduler/src/main/java/com/rajendarreddyj/scheduler/TaskScheduler.java
+++ java-projects/scheduler/src/main/java/com/rajendarreddyj/scheduler/TaskScheduler.java
@@ -1,20 +1,20 @@
 package com.rajendarreddyj.scheduler;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.TimerTask;
-
-import javax.net.ssl.HttpsURLConnection;
 
 import com.rajendarreddyj.scheduler.bean.ServerVariables;
 
@@ -28,6 +28,8 @@ public class TaskScheduler extends TimerTask {
     private String fileExtension;
     private String archiveFileExtension;
     private String webServiceUrl;
+    private String tempFilePath;
+    private long filePointer;
 
     /**
      * @param filePath
@@ -51,20 +53,17 @@ public class TaskScheduler extends TimerTask {
         System.out.println("Scheduler started @ " + new Date());
         // Read File
         File[] listOfFiles = this.finder(this.filePath, this.fileExtension);
+        if (listOfFiles != null) {
+            for (File file : listOfFiles) {
+                this.tempFilePath = file.getAbsolutePath();
+                // this.readFile(file);
+                this.readFileWithPointer(file);
+                this.renameFile(file);
+                this.deleteTempFile(tempFilePath);
 
-        for (File file : listOfFiles) {
-            List<ServerVariables> serverVariablesList = this.readFile(file);
-            System.out.println("Call WS");
-            System.out.println(serverVariablesList.toString());
-            try {
-                // WS Call
-                this.sendPost(this.webServiceUrl, serverVariablesList);
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
             }
-            this.renameFile(file);
-
+        } else {
+            System.out.println("Correct File Path in properties");
         }
     }
 
@@ -74,55 +73,49 @@ public class TaskScheduler extends TimerTask {
      * @param serverVariablesList
      * @throws Exception
      */
-    private void sendPost(final String url, final List<ServerVariables> serverVariablesList) {
+    private void sendPost(final String url, final ServerVariables serverVariables) {
         int retry = 3;
-        for (ServerVariables serverVariables : serverVariablesList) {
-            // Retry 3 times for Failure for Each Record Insertion
-            for (int i = 0; i < retry; i++) {
-                System.out.println("Trying " + i);
-                try {
-                    URL obj = new URL(url);
-                    HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+        // Retry 3 times for Failure for Each Record Insertion
+        for (int i = 0; i < retry; i++) {
+            System.out.println("Trying " + i);
+            try {
+                URL obj = new URL(url);
+                HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-                    // add reuqest header
-                    con.setRequestMethod("POST");
-                    con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+                // add reuqest header
+                con.setRequestMethod("GET");
+                con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 
-                    String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+                // Send post request
+                con.setDoOutput(true);
+                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+                wr.writeBytes("");
+                wr.flush();
+                wr.close();
 
-                    // Send post request
-                    con.setDoOutput(true);
-                    DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-                    wr.writeBytes(urlParameters);
-                    wr.flush();
-                    wr.close();
+                int responseCode;
+                responseCode = con.getResponseCode();
 
-                    int responseCode;
-                    responseCode = con.getResponseCode();
-
-                    if (responseCode == 200) {
-                        break;
-                    }
-                    System.out.println("\nSending 'POST' request to URL : " + url);
-                    System.out.println("Post parameters : " + urlParameters);
-                    System.out.println("Response Code : " + responseCode);
-
-                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                    String inputLine;
-                    StringBuffer response = new StringBuffer();
-
-                    while ((inputLine = in.readLine()) != null) {
-                        response.append(inputLine);
-                    }
-                    in.close();
-                    System.out.println(response.toString());
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
+                if (responseCode == 200) {
+                    break;
                 }
-                // print result
+                System.out.println("\nSending 'GET' request to URL : " + url);
+                System.out.println("Response Code : " + responseCode);
 
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                System.out.println(response.toString());
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
+            // print result
 
         }
 
@@ -135,7 +128,7 @@ public class TaskScheduler extends TimerTask {
         File file2 = new File(file.getAbsoluteFile() + this.archiveFileExtension);
         boolean success = file.renameTo(file2);
         if (success) {
-            System.out.println("File Renamed");
+            System.out.println("Processing File Renamed");
         }
     }
 
@@ -160,14 +153,13 @@ public class TaskScheduler extends TimerTask {
      * @param file
      * @return
      */
-    private List<ServerVariables> readFile(final File file) {
+    private void readFile(final File file) {
         BufferedReader br = null;
         String line = "";
         String lineSplitBy = "\\|";
-        List<ServerVariables> serverVariablesList = new ArrayList<>();
         try {
-
-            br = new BufferedReader(new FileReader(file));
+            FileReader fileReader = new FileReader(file);
+            br = new BufferedReader(fileReader);
             while ((line = br.readLine()) != null) {
                 ServerVariables serverVariables = new ServerVariables();
                 // use pipe as separator
@@ -196,7 +188,15 @@ public class TaskScheduler extends TimerTask {
                 if (values.length > 7) {
                     serverVariables.setServerSoftware(values[7]);
                 }
-                serverVariablesList.add(serverVariables);
+                System.out.println("Call WS");
+                System.out.println(serverVariables);
+                try {
+                    // WS Call
+                    this.sendPost(this.webServiceUrl, serverVariables);
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
 
         } catch (FileNotFoundException e) {
@@ -215,7 +215,149 @@ public class TaskScheduler extends TimerTask {
                 }
             }
         }
-        return serverVariablesList;
     }
 
+    /**
+     * @param file
+     * @return
+     */
+    private void readFileWithPointer(final File file) {
+        String line = "";
+        String lineSplitBy = "\\|";
+        long len = file.length();
+
+        System.out.println("seeking position " + this.filePointer);
+        try {
+            // To Save and Retrieve pointer temp file is used.
+            RandomAccessFile raf = new RandomAccessFile(file, "r");
+            this.filePointer = this.readPointerFromTempFile();
+            if (this.filePointer < len) {
+                raf.seek(this.filePointer);
+            } else {
+                raf.seek(0);
+            }
+            while ((line = raf.readLine()) != null) {
+                ServerVariables serverVariables = new ServerVariables();
+                // use pipe as separator
+                String[] values = line.split(lineSplitBy);
+                if (values.length > 0) {
+                    serverVariables.setHttpReferer(values[0]);
+                }
+                if (values.length > 1) {
+                    serverVariables.setHttpUserAgent(values[1]);
+                }
+                if (values.length > 2) {
+                    serverVariables.setRemoteAddress(values[2]);
+                }
+                if (values.length > 3) {
+                    serverVariables.setRemoteHost(values[3]);
+                }
+                if (values.length > 4) {
+                    serverVariables.setRequestMethod(values[4]);
+                }
+                if (values.length > 5) {
+                    serverVariables.setServerName(values[5]);
+                }
+                if (values.length > 6) {
+                    serverVariables.setServerPort(values[6]);
+                }
+                if (values.length > 7) {
+                    serverVariables.setServerSoftware(values[7]);
+                }
+                System.out.println("Call WS ");
+                System.out.println(serverVariables);
+                try {
+                    // WS Call
+                    this.sendPost(this.webServiceUrl, serverVariables);
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                this.filePointer = raf.getFilePointer();
+                System.out.println("Set Pointer " + raf.getFilePointer());
+                this.createTempFileForPointer(raf.getFilePointer());
+            }
+            raf.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+        }
+    }
+
+    /**
+     * @param pointer
+     */
+    private void createTempFileForPointer(final long pointer) {
+        BufferedWriter bw = null;
+        try {
+
+            // create a temp file
+            File temp = new File(this.tempFilePath + ".tmp");
+            temp.createNewFile();
+            // write it
+            bw = new BufferedWriter(new FileWriter(temp));
+            bw.write(Long.toString(pointer));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bw != null) {
+                    bw.close();
+                }
+            } catch (IOException ex) {
+                // TODO Auto-generated catch block
+                ex.printStackTrace();
+            }
+
+        }
+    }
+
+    /**
+     * @return
+     */
+    private long readPointerFromTempFile() {
+        BufferedReader br = null;
+        FileReader fr = null;
+        long pointer = 0;
+        try {
+            fr = new FileReader(this.tempFilePath + ".tmp");
+            br = new BufferedReader(fr);
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null) {
+                pointer = Long.parseLong(sCurrentLine.trim());
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (IOException ex) {
+                // TODO Auto-generated catch block
+                ex.printStackTrace();
+            }
+        }
+        return pointer;
+    }
+
+    /**
+     * @param file
+     */
+    private void deleteTempFile(final String tempFilePath) {
+        File file = new File(tempFilePath + ".tmp");
+        boolean success = file.delete();
+        if (success) {
+            System.out.println("Temp File Deleted");
+        }
+    }
 }
