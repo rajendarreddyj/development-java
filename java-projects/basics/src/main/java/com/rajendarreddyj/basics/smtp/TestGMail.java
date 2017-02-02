1184e53b63b821c1d2929b3cebfd9dec96d05a28
rajendarreddy jagapathi <rajendarreddyj@gmail.com>
2017-02-01 22:32:36 -0600

    Smtp java mail program

---
+++ java-projects/basics/src/main/java/com/rajendarreddyj/basics/smtp/TestGMail.java
@@ -0,0 +1,37 @@
+package com.rajendarreddyj.basics.smtp;
+
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import com.sun.mail.smtp.SMTPTransport;
+
+public class TestGMail {
+    public static void main(final String[] args) {
+        Properties props = System.getProperties();
+        props.put("mail.smtps.host", "smtp.gmail.com");
+        props.put("mail.smtps.auth", "true");
+        Session session = Session.getInstance(props, null);
+        Message msg = new MimeMessage(session);
+        try {
+            msg.setFrom(new InternetAddress("test@gmail.com"));;
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("test@gmail.com", false));
+            msg.setSubject("RajendarTEST " + System.currentTimeMillis());
+            msg.setText("Hi rajendar, Scheduler started running");
+            msg.setHeader("TEST EMAIL", "Rajendar's program");
+            msg.setSentDate(new Date());
+            SMTPTransport t = (SMTPTransport) session.getTransport("smtps");
+            t.connect("smtp.gmail.com", "test@gmail.com", "password");
+            t.sendMessage(msg, msg.getAllRecipients());
+            t.close();
+            System.out.println("Response: " + t.getLastServerResponse());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ java-projects/basics/src/main/java/com/rajendarreddyj/basics/smtp/TestYahooMail.java
@@ -0,0 +1,52 @@
+package com.rajendarreddyj.basics.smtp;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class TestYahooMail {
+    public static void main(final String[] args) {
+        String smtpHost = "smtp.mail.yahoo.com";
+        String from = "username";
+        String pass = "password";
+        Properties props = System.getProperties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pass);
+        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.auth", "true");
+
+        String[] to = { "test@gmail.com" };
+
+        Session session = Session.getDefaultInstance(props, null);
+        MimeMessage message = new MimeMessage(session);
+        try {
+            message.setFrom(new InternetAddress(from));
+
+            InternetAddress[] toAddress = new InternetAddress[to.length];
+
+            // To get the array of addresses
+            for (int i = 0; i < to.length; i++) {
+                toAddress[i] = new InternetAddress(to[i]);
+            }
+            System.out.println(Message.RecipientType.TO);
+
+            for (int i = 0; i < toAddress.length; i++) {
+                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+            }
+            message.setSubject("sending in a group");
+            message.setText("Test Email Java SMTP");
+            Transport transport = session.getTransport("smtp");
+            transport.connect(smtpHost, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
