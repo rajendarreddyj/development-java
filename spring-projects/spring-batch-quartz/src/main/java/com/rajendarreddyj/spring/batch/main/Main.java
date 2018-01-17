3dd85876ae161d210d144592638bcab2baa14876
rajendarreddyj <rajendarreddyj@gmail.com>
2018-01-17 02:10:31 -0600

    spring batch quartz

--- spring-projects/spring-batch-quartz/src/main/java/com/rajendarreddyj/spring/batch/main/Main.java
+++ spring-projects/spring-batch-quartz/src/main/java/com/rajendarreddyj/spring/batch/main/Main.java
@@ -1,13 +1,17 @@
 package com.rajendarreddyj.spring.batch.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author rajendarreddy.jagapathi
  *
  */
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.rajendarreddyj")
 public class Main {
     public static void main(final String[] args) {
         SpringApplication.run(Main.class, args);
